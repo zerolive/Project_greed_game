77cc8e0c9f8c3d449fbe24261b2060e18635ed11
zerolive <zerolive@gmail.com>
2015-08-26 13:04:13 +0200

    finished greed.rb

--- finalscore.rb
+++ finalscore.rb
@@ -1,5 +1,11 @@
-scoreplayers = [1, 2, 3, 4, 5]
-
+#Mostrando la tabla de puntuaciones.
+p "SCORE TABLE"
 scoreplayers.each_with_index do |values, index|
-p "Player #{(index+1)} has #{values}"
+	if values != 0
+		p "Player #{(index+1)} has #{values}"
+	end
 end
+#Descubriendo el ganador
+winner = scoreplayers.rindex(scoreplayers.max)
+#Mostrando al ganador
+p "Congratulations PLAYER #{winner +1}, you won with #{scoreplayers[winner]} points." 
--- greed.rb
+++ greed.rb
@@ -142,5 +142,11 @@ p "#{scoreplayers.map { |i| "'" + i.to_s + "'" }.join(",")}"
 #Mostrando la tabla de puntuaciones.
 p "SCORE TABLE"
 scoreplayers.each_with_index do |values, index|
-p "Player #{(index+1)} has #{values}"
+	if values != 0
+		p "Player #{(index+1)} has #{values}"
+	end
 end
+#Descubriendo el ganador
+winner = scoreplayers.rindex(scoreplayers.max)
+#Mostrando al ganador
+p "Congratulations PLAYER #{winner +1}, you won with #{scoreplayers[winner]} points." 
