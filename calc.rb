25d6612cc447865cbea73bdea249bbdf627394fa
zerolive <zerolive@gmail.com>
2015-08-26 11:05:21 +0200

    terminado calc.rb

--- calc.rb
+++ calc.rb
@@ -2,6 +2,7 @@ class Calc
 	def sum(dices)
 		score = 0
 		number = [0, 0, 0, 0, 0, 0]
+		#Mirando las tiradas.
 		dices.each do |values|
 			if values == 1
 				number[0] += 1
@@ -17,9 +18,46 @@ class Calc
 				number[5] +=1
 			end
 		end
-	p "#{number.map { |i| "'" + i.to_s + "'" }.join(",")}"
+		#Calculando los 1
+		if number[0] == 1
+			score += 100
+		elsif number[0] == 2
+			score += 200
+		elsif number[0] == 3
+			score += 1000
+		elsif number[0] == 4
+			score += 1100
+		elsif number[0] == 5
+			score += 1200
+		end
+		#calculando los 2
+		if number[1] == 3
+			score += 200
+		end
+		#calculando los 3
+		if number[2] == 3
+			score += 300
+		end
+		#calculando los 4
+		if number[3] == 3
+			score += 400
+		end
+		#calculando los 5
+		if number[4] == 1
+			score += 50
+		elsif number[4] == 2
+			score += 100
+		elsif number[4] == 3
+			score += 500
+		elsif number[4] == 4
+			score += 550
+		elsif number[4] == 5
+			score += 600
+		end
+		#calculando los 6
+		if number[5] == 3
+			score += 600
+		end
+	return score
 	end
 end
-
-prueba = Calc.new
-p prueba.sum([1, 1, 6, 4, 3])
